08068c82df9441a8739e2fe83b6f560e9557c386
vagrant <vagrant@localhost.localdomain>
2017-07-02 18:44:32 +0000

    most of chapter four done

---
+++ chapter4/static_content.py
@@ -0,0 +1,9 @@
+from twisted.internet import reactor
+from twisted.web.server import Site
+from twisted.web.static import File
+
+resource = File('/var/www/my-site')
+factory = Site(resource)
+reactor.listenTCP(8000, factory)
+reactor.run()
+
---
+++ chapter4/static_dispatch.py
@@ -0,0 +1,13 @@
+from twisted.internet import reactor
+from twisted.web.server import Site
+from twisted.web.static import File
+
+root = File('/var/www/my-site')
+root.putChild("doc", File("/usr/share/doc"))
+root.putChild("logs", File("/var/log/mysitelogs"))
+
+factory = Site(root)
+reactor.listenTCP(8000, factory)
+reactor.run()
+
+
